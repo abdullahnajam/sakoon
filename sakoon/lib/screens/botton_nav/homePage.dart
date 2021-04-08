92ff929d06d2c1e698314caad7905359db6806ce
Abdullah Najam <36923260+abdullahnajam@users.noreply.github.com>
2021-04-08 12:56:37 +0500

    home

--- sakoon/lib/screens/botton_nav/homePage.dart
+++ sakoon/lib/screens/botton_nav/homePage.dart
@@ -9,130 +9,131 @@ static String routename='/homepage';
 _HomePageState createState() => new _HomePageState();
 }
 class _HomePageState extends State<HomePage> {
-  PageController _controller = PageController(
-    initialPage: 0,
-  );
-  @override
-  void initState() {
-    super.initState();
 
-  }
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    /*24 is for notification bar on Android*/
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
+    final double itemWidth = size.width / 2;
     return Scaffold(
-      body: ListView.builder(itemBuilder: (context, index) {
-        return Container(
-          height: MediaQuery.of(context).size.height,
-          child: ListView(
-            children: <Widget>[
+      body: ListView(
+        children: [
+          Stack(
+            children: [
               Container(
-                height: 375,
-                child: Stack(
-                  children: <Widget>[
-                    Container(
-                      height: 325,
-                      child: PageView.builder(
-                        itemBuilder: (context, position) {
-                          return Container(
-                              padding: EdgeInsets.only(bottom: 50),
-                              height: 225,
-                              decoration: new BoxDecoration(
-                                  image: new DecorationImage(
-                                      fit: BoxFit.fill,
-                                      image: AssetImage('assets/images/logo.png'),
-                                  )
-                              ),
-                              width: MediaQuery
-                                  .of(context)
-                                  .size
-                                  .width,
+                padding: EdgeInsets.all(10),
+                height: MediaQuery.of(context).size.height*0.3,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.centerRight,
+                      end: Alignment.centerLeft,
+                      colors: [
+                        Color(0xfffffbef),
+                        Color(0xffdbf8fe),
 
-                              child: Align(
-                                  alignment: Alignment.bottomCenter,
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: <Widget>[
-                                      Text('',
-                                        textAlign: TextAlign.start,
-                                        style: TextStyle(color: Colors.black,
-                                            fontSize: 50,
-                                            fontWeight: FontWeight.w600),),
-
-                                    ],
-                                  )
-                              )
-                          );
-                        },
-                        itemCount: 3, // Can be null
-                      )
+                      ],
+                    )
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding:EdgeInsets.all(10),
+                          child: Image.asset('assets/images/menu.png',width: 25,height: 25,),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(100)
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 15,right: 15),
+                          height: 40,
+                          child: Row(
+                            children: [
+                              Icon(Icons.map_outlined,color: Colors.white,),
+                              SizedBox(width: 7,),
+                              Text("MAP",style: TextStyle(color: Colors.white),)
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                              color: Colors.blueAccent,
+                              borderRadius: BorderRadius.circular(30)
+                          ),
+                        )
+                      ],
                     ),
-
-
-                    Card(
-
-                      margin: EdgeInsets.only(top: 300, left: 25, right: 25),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius
-                          .circular(25),),
-                      clipBehavior: Clip.antiAliasWithSaveLayer,
-                      elevation: 1,
-                      child: Row(
-                        children: <Widget>[
-                          IconButton(
-                              icon: Icon(Icons.search, color: Colors.grey[600]),
-                              onPressed: () {
-
-                              }),
-                          Expanded(
-                            child: Text("Search",style: TextStyle(
-                                color: Colors.grey[600], fontSize: 18),)
-                          )
+                    SizedBox(height: 30,),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.place_outlined,color: Colors.grey[500],size: 20,),
+                              Text("My Location",style: TextStyle(color: Colors.grey[500]),)
+                            ],
+                          ),
+                          Text("CHOOSE SERVICES YOU",style: TextStyle(color: Colors.black,fontSize: 25,fontWeight: FontWeight.w900),),
+                          Text("WANT TO NEED",style: TextStyle(color: Colors.black,fontSize: 25,fontWeight: FontWeight.w900),)
                         ],
                       ),
-                    ),
-
+                    )
                   ],
                 ),
               ),
-              //SizedBox(height: 20,),
-              Container(
-                margin: EdgeInsets.all(25),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    DefaultButton(
-                      text: "Fresh Work",
-                      press: () {
-                        Navigator.pushNamed(context,HomeMaintanance.routeName);
-                      },
-                    ),
-                    SizedBox(height: 50,),
-                    DefaultButton(
-                      text: "Maintenance",
-                      press: () {
-                        Navigator.pushNamed(context,HomeMaintanance.routeName);
-                      },
-                    ),
-                    SizedBox(height: 50,),
-                    DefaultButton(
-                      text: "Home Appliances",
-                      press: () {
-                      },
-                    ),
-                    SizedBox(height: 50,),
-                  ],
+              Align(
+                child: Container(
+                  padding: EdgeInsets.only(left: 15,right: 15),
+                  height: 40,
+                  width: 120,
+                  margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.27,right: 10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.search,color: Colors.white,),
+                      SizedBox(width: 7,),
+                      Text("Search",style: TextStyle(color: Colors.white),)
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                      color: Color(0xffffcb51),
+                      borderRadius: BorderRadius.circular(30)
+                  ),
                 ),
+                alignment: Alignment.centerRight,
               )
             ],
           ),
-        );
-      }),
+          Container(
+            height: MediaQuery.of(context).size.height*0.7,
+            child: GridView.count(
+                shrinkWrap: true,
+                crossAxisCount: 2,
+                crossAxisSpacing: 1.0,
+                mainAxisSpacing: 1.0,
+                children: List.generate(2, (index) {
+                  return Center(
+                    child: Container(
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          Image.asset('assets/images/logo.png',width: 80,height: 80,),
+                          Text("Title",style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.w500),)
+                        ],
+                      ),
+                    ),
+                  );
+                }
+                )
+            ),
+          )
+   
+    
+        ],
+      ),
     );
   }
 }
